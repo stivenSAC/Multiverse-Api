8c53ec8f4207c0e05b008d2a1008476448ba0f63
stivenSAC <stiven.alvarezco01@gmail.com>
2025-09-04 14:44:15 -0500

    primer commit

---
+++ src/main/java/com/example/Multiverse/MultiverseApplication.java
@@ -0,0 +1,15 @@
+package com.example.Multiverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiverseApplication.class, args);
+
+		//hola
+	}
+
+}
---
+++ src/test/java/com/example/Multiverse/MultiverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Multiverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiverseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
