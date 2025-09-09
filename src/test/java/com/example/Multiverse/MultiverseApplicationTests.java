90a29e18f895228eb8072c0e070077d3da634537
stivenSAC <stiven.alvarezco01@gmail.com>
2025-09-09 10:05:07 -0500

    Se agrego h2.driver en el test del respositori

--- src/test/java/com/example/Multiverse/MultiverseApplicationTests.java
+++ src/test/java/com/example/Multiverse/MultiverseApplicationTests.java
@@ -7,6 +7,8 @@ import org.springframework.test.context.TestPropertySource;
 @SpringBootTest
 @TestPropertySource(properties = {
     "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
     "spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class MultiverseApplicationTests {
